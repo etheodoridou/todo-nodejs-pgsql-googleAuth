33e50017fcaf6b801f09ee51c3735ffd195fb8ea
Eva Theodoridou <23189351+etheodoridou@users.noreply.github.com>
2024-08-29 14:10:30 -0700

    Update queries.js

--- server/queries.js
+++ server/queries.js
@@ -20,6 +20,7 @@ const createDB = `CREATE TABLE todoitems(
 const initializeDatabase = (callback) => {
   console.log('Initialize database');
   pool.query(selectExists, (error, result) => {
+    console.log(result);
     if (error) {
       console.log(`Failed to query todoitems table: ${error}`);
       callback();
